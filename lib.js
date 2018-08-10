a221b4d66919b165e23e4e4429d6f2d406597f9c
Jan Koudijs <theappgineer@gmail.com>
2018-08-10 11:05:51 +0200

    Fixed repository reloading bug
    (cherry picked from commit 6adf010408a262ae14293d0f502cb869a4b00ff9)

--- lib.js
+++ lib.js
@@ -286,6 +286,8 @@ function _load_repository() {
     let local_repos = extension_root + repos_dir;
     let values = [];
 
+    repos.length = 0;       // Cleanup first
+
     repos.push(repos_system);
     _add_to_repository(main_repo);
 
